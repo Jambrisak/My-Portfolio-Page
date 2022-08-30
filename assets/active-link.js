dca5a94a5c7525f85605b363f8ed08eff14c05ec
Philip Jambrisak <jambrisak@gmail.com>
2022-08-30 08:58:48 +0200

    Create script.js

---
+++ assets/active-link.js
@@ -0,0 +1,9 @@
+document.querySelectorAll(".nav-item").forEach((ele) =>
+  ele.addEventListener("click", function (event) {
+    event.preventDefault();
+    document
+      .querySelectorAll(".nav-item")
+      .forEach((ele) => ele.classList.remove("active"));
+    this.classList.add("active")
+  })
+);
