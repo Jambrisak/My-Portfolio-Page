12b7f833c481edabb37af45caf604cdb78c6596a
Philip Jambrisak <jambrisak@gmail.com>
2022-09-30 18:47:38 +0200

    final commit on new page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,19 @@
-/*===== MENU SHOW Y HIDDEN =====*/
-
 const navMenu = document.getElementById('nav-menu'),
     toggleMenu = document.getElementById('nav-toggle'),
     closeMenu = document.getElementById('nav-close')
-
-// SHOW
 toggleMenu.addEventListener('click', ()=>{
     navMenu.classList.toggle('show')
 })
-
-// HIDDEN
 closeMenu.addEventListener('click', ()=>{
     navMenu.classList.remove('show')
 })
-
-/*===== ACTIVE AND REMOVE MENU =====*/
 const navLink = document.querySelectorAll('.nav__link');   
 
 function linkAction(){
-  /*Remove menu mobile*/
   navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*===== SCROLL SECTIONS ACTIVE LINK =====*/
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
@@ -44,10 +34,8 @@ function scrollActive(){
 
 window.addEventListener('scroll', scrollActive)
 
-/*==================== SHOW SCROLL TOP ====================*/ 
 function scrollTop(){
     const scrollTop = document.getElementById('scroll-top');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
     if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
